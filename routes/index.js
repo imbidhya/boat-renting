9aba6945256a6ebd416bb369852558c2d77f93c2
saloni <newa30saloni@gmail.com>
2022-03-10 00:45:59 +0545

    changes-database

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 var express = require('express');
-const booking = require('../model/booking');
+// const booking = require('../model/booking');
 var router = express.Router();
 var Booking = require('../model/booking');
 
@@ -18,8 +18,10 @@ router.get("/gotobookingpage",function(req,res,next){  //it get the data from ad
 router.get("/Save-data",function(req,res,next){  //it get the data from addtodo list
   res.render("bookafter");
 });
-router.post("/save-booking",function(req,res,next){   //it save the data from the form
-  // console.log(req.body);  
+
+router.post("/save-booking", function(req,res,next){   //it save the data from the form
+  console.log("bodyyyyyyyyyyyyy 1234", req.body); 
+
   const booking=new Booking({
     name:req.body.name,
     contact : req.body.contact, 
@@ -28,11 +30,38 @@ router.post("/save-booking",function(req,res,next){   //it save the data from th
     checkOut:req.body.checkOut
   });
 
+  // console.log('booooooooooooooooking', booking.name);
+  // const savedbooking = await booking.save();
+  // console.log("your booking is",savedbooking.name,savedbooking._id);
+  // res.redirect(`/bookingInfo/`+ savedbooking._id);
+
   const promise = booking.save();
   promise.then((booking)=>{
-    console.log("your booking is",booking);
-    res.redirect('/gotobookingpage');
+    console.log("your booking is",booking,booking._id);
+    // res.render("display",{booking});
+    // booking.push({...req.body,id: booking._id})
+    res.redirect(`/bookingInfo/`+booking._id);
   })
 });
+
+router.get("/bookingInfo/:id", async function(req,res,next){  //it get the data from addtodo list
+  const bookingDetails = await Booking.findOne({_id:req.params.id});
+  console.log("bookingDetails",bookingDetails)
+  res.render("display",{bookingDetails:bookingDetails});
+
+});
+
+router.post('/booking-Info/:id',async function(req,res,next){
+  await booking.findOne({_id:req.params.id},{
+    $set:{
+      name: req.body.name,
+      contact: req.body.contact,
+      ticket: req.body.ticket,
+      startTime:req.body.startTime,
+      endTime: req.body.endTime
+    }
+  })
+  res.redirect(`/bookingInfo/`+booking._id);
+})
 
 module.exports = router;
