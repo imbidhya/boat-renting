3e6a043cd859c7287809342da9a18d48cbba02e6
imbidhya <bidhyapoudel436@gmail.com>
2022-03-09 15:10:22 +0545

    UI changes [in-progress]

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use('/users', usersRouter);
 app.use(function(req, res, next) {
   next(createError(404));
 });
-
+app.use(express.static(__dirname + '/public'));
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
