286b76d74ab6db0e0f063b2eaba987f4f2b7b827
imbidhya <bidhyapoudel436@gmail.com>
2022-03-10 01:34:18 +0545

    bidhya's homepage integration

---
+++ model/check.js
@@ -0,0 +1,12 @@
+var mongoose = require('mongoose');
+
+const checkSchema = mongoose.Schema({
+    date: date,
+    Time:
+    day:
+    
+
+   
+});
+
+module.exports = mongoose.model('boatbooking',bookingSchema)
