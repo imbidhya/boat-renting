f9d0643295086dd86296cd5d96d31e66e35a0dc4
Samiksha Paudel <samikshapaudel22@gmail.com>
2022-03-10 12:38:53 +0545

    updates

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@ var express = require('express');
 const booking = require('../model/booking');
 var router = express.Router();
 var Booking = require('../model/booking');
+const fetch = require('node-fetch')
+let alert = require('alert'); 
 
 
 /* GET home page. */
@@ -16,6 +18,7 @@ router.get('/', function(req, res, next) {
 
 
 router.get("/gotobookingpage",function(req,res,next){  //it get the data from addtodo list
+  alert("message")
   res.render("bookafter");
 });
 
@@ -42,10 +45,6 @@ router.post("/save-booking", function(req,res,next){   //it save the data from t
     checkOut:req.body.checkOut
   });
 
-  // console.log('booooooooooooooooking', booking.name);
-  // const savedbooking = await booking.save();
-  // console.log("your booking is",savedbooking.name,savedbooking._id);
-  // res.redirect(`/bookingInfo/`+ savedbooking._id);
 
   const promise = booking.save();
   promise.then((booking)=>{
@@ -58,8 +57,12 @@ router.post("/save-booking", function(req,res,next){   //it save the data from t
 
 router.get("/bookingInfo/:id", async function(req,res,next){  //it get the data from addtodo list
   const bookingDetails = await Booking.findOne({_id:req.params.id});
-  console.log("bookingDetails",bookingDetails)
-  res.render("display",{bookingDetails:bookingDetails});
+  fetch('https://api.openweathermap.org/data/2.5/onecall?lat=28.2096&lon=83.9856&appid=780a4551ff3e6ac4892ab54ec1e701ec&units=metric')
+  .then(res => res.json())
+  .then(weatherData => res.render('bookingDetails',{weatherData: weatherData,bookingDetails:bookingDetails}))
+  .catch(err => console.log(err));
+  // console.log("bookingDetails",bookingDetails)
+  // res.render("bookingDetails",{bookingDetails:bookingDetails});
 
 });
 
